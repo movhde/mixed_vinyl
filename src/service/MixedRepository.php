ced2a2857176cb06a0abaf7c83bbf87f084cda2c
movahede salehivaziri <movahede@movahedes-MacBook-Air.local>
2024-04-23 22:47:41 +0330

    Add a new custom service

---
+++ src/service/MixedRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MixedRepository
+{
+  public function __construct(
+    private CacheInterface $cache,
+    private HttpClientInterface $githubContentClient,
+    #[Autowire('%kernel.debug%')]
+    private bool $isDebug,
+    #[Autowire(service: 'twig.command.debug')]
+    private DebugCommand $twigCommand
+  ) {
+  }
+
+  public function findAll()
+  {
+    // $output = new BufferedOutput();
+    // $this->twigCommand->run(new ArrayInput([]), $output);
+    // dd($output);
+
+    return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
+      $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
+      $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
+
+      return $response->toArray();
+    });
+  }
+}
